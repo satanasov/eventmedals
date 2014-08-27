4fd39530cb250c0506acbea3202cdd96d41d400d
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-27 14:57:19 +0300

    Install test

--- tests/functional/eventmedals_main_test.php
+++ tests/functional/eventmedals_main_test.php
@@ -18,6 +18,11 @@ class eventmedals_main_test extends eventmedals_base
 	public function test_install()
 	{
 		$this->login();
-		$this->assertContains('123', '123');
+		
+		// Test creating topic and post to test
+		$post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
+		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
+		
+		$this->assertContains('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
 	}
 }
