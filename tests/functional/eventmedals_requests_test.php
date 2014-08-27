91725205b57f4a8e7ed13476d329eb7176c668e4
Stanislav Atanasov <lucifer@anavaro.com>
2014-08-27 11:18:03 +0300

    tests

---
+++ tests/functional/eventmedals_requests_test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*
+* EventMedals test
+*
+* @copyright (c) 2014 Stanislav Atanasov
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace anavaro\eventmedals\tests\functional;
+
+/**
+* @group functional
+*/
+class eventmedals_main_test extends eventmedals_base
+{
+
+}
