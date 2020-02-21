0c0c56632f7cac4c9548084234555c41f77f55ed
Stanislav Atanasov <s.atanasov@anavaro.com>
2020-02-21 17:10:11 +0200

    Cleanup some code

--- controller/ajaxify.php
+++ controller/ajaxify.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Anavaro.com Zebra Enchance
+* @package Anavaro.com Event medals
 * @copyright (c) 2013 Lucifer
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -16,34 +16,70 @@ namespace anavaro\eventmedals\controller;
 class ajaxify
 {
 	/**
-	* Constructor
-	* NOTE: The parameters of this method must match in order and type with
-	* the dependencies defined in the services.yml file for this service.
-	*
-	* @param \phpbb\auth		$auth		Auth object
-	* @param \phpbb\cache\service	$cache		Cache object
-	* @param \phpbb\config	$config		Config object
-	* @param \phpbb\db\driver	$db		Database object
-	* @param \phpbb\request	$request	Request object
-	* @param \phpbb\template	$template	Template object
-	* @param \phpbb\user		$user		User object
-	* @param \phpbb\content_visibility		$content_visibility	Content visibility object
-	* @param \phpbb\controller\helper		$helper				Controller helper object
-	* @param string			$root_path	phpBB root path
-	* @param string			$php_ext	phpEx
-	*/
-	public function __construct(\phpbb\auth\auth $auth, \phpbb\cache\service $cache, \phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user, \phpbb\controller\helper $helper, $root_path, $php_ext, $table_prefix)
+	 * @var \phpbb\auth|\phpbb\auth\auth
+	 */
+	protected $auth;
+
+	/**
+	 * @var \phpbb\db\driver|\phpbb\db\driver\driver_interface
+	 */
+	protected $db;
+
+	/**
+	 * @var \phpbb\request|\phpbb\request\request
+	 */
+	protected $request;
+
+	/**
+	 * @var \phpbb\template|\phpbb\template\template
+	 */
+	protected $template;
+
+	/**
+	 * @var \phpbb\user
+	 */
+	protected $user;
+
+	/**
+	 * @var \phpbb\controller\helper
+	 */
+	protected $helper;
+
+	/**
+	 * @var string
+	 */
+	protected $root_path;
+
+	/**
+	 * @var string
+	 */
+	protected $table_prefix;
+
+	/**
+	 * Constructor
+	 * NOTE: The parameters of this method must match in order and type with
+	 * the dependencies defined in the services.yml file for this service.
+	 *
+	 * @param \phpbb\auth $auth                Auth object
+	 *                                         * @param \phpbb\config $config Config object
+	 * @param \phpbb\db\driver $db             Database object
+	 *                                         * @param \phpbb\request $request Request object
+	 * @param \phpbb\template $template        Template object
+	 *                                         * @param \phpbb\user $user User object
+	 * @param \phpbb\controller\helper $helper Controller helper object
+	 *                                         * @param string $root_path phpBB root path
+	 * @param string $php_ext phpEx
+	 * @param string $table_prefix	Table refix
+	 */
+	public function __construct(\phpbb\auth\auth $auth, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, \phpbb\template\template $template, \phpbb\user $user, \phpbb\controller\helper $helper, $root_path, $table_prefix)
 	{
 		$this->auth = $auth;
-		$this->cache = $cache;
-		$this->config = $config;
 		$this->db = $db;
 		$this->request = $request;
 		$this->template = $template;
 		$this->user = $user;
 		$this->helper = $helper;
 		$this->root_path = $root_path;
-		$this->php_ext = $php_ext;
 		$this->table_prefix = $table_prefix;
 	}
 
