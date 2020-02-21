cd39f4ce6840c62e33b87c12ac390d1c04140507
Stanislav Atanasov <s.atanasov@anavaro.com>
2020-02-21 17:19:29 +0200

    Less code smell in tests

--- tests/functional/eventmedals_acp_edit_test.php
+++ tests/functional/eventmedals_acp_edit_test.php
@@ -21,9 +21,9 @@ class eventmedals_acp_edit_test extends eventmedals_base
 		//add medals
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
 		$this->assertContainsLang('ERR_NO_MEDALS', $crawler->filter('html')->text());
 		$this->logout();
@@ -33,35 +33,35 @@ class eventmedals_acp_edit_test extends eventmedals_base
      */
 	public function test_acp_edit_build_medals()
 	{
-		
+
 		$owner_id = $this->get_user_id('admin');
 		$type = 1;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser1');
 		$type = 2;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser2');
 		$type = 3;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser3');
 		$type = 4;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 	}
 	/**
      * @depends test_acp_edit_build_medals
@@ -70,20 +70,20 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['username'] = 'testuser4';
 		$form['event_edit_type'] = 'user';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('ERR_NO_USER', $crawler->filter('html')->text());
 		$this->logout();
-		
+
 	}
 	/**
      * @depends test_acp_edit_user_no_user
@@ -92,20 +92,20 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->create_user('testuser4');
 		$this->add_user_group('NEWLY_REGISTERED', array('testuser1'));
-		
+
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['username'] = 'testuser4';
 		$form['event_edit_type'] = 'user';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('ERR_USER_NO_MEDALS', $crawler->filter('html')->text());
 		$this->logout();
 	}
@@ -116,31 +116,31 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->assertEquals(4, $this->medals_for_event($this->get_topic_id('Test Topic 1')));
 		$this->assertEquals(1, $this->medals_for_user($this->get_user_id('testuser3')));
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['username'] = 'testuser3';
 		$form['event_edit_type'] = 'user';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['delete'] = array($this->get_topic_id('Test Topic 1'));
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->filter('html')->text());
-		
+
 		$this->assertEquals(0, $this->medals_for_user($this->get_user_id('testuser3')));
-		
+
 		$this->assertEquals(3, $this->medals_for_event($this->get_topic_id('Test Topic 1')));
-		
+
 		$this->logout();
 	}
 	/**
@@ -150,27 +150,27 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->assertEquals(3, $this->medal_type($this->get_user_id('testuser2'), $this->get_topic_id('Test Topic 1')));
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['username'] = 'testuser2';
 		$form['event_edit_type'] = 'user';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['events'] = array($this->get_topic_id('Test Topic 1') => array('select' => 2));
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->filter('html')->text());
 		$this->assertEquals(2, $this->medal_type($this->get_user_id('testuser2'), $this->get_topic_id('Test Topic 1')));
-		
+
 		$this->logout();
 	}
 	/**
@@ -182,41 +182,41 @@ class eventmedals_acp_edit_test extends eventmedals_base
 		//add medals
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
 		$this->assertContainsLang('ERR_NO_MEDALS', $crawler->filter('html')->text());
 		$this->logout();
-		
+
 		$owner_id = $this->get_user_id('admin');
 		$type = 1;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser1');
 		$type = 2;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser2');
 		$type = 3;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser3');
 		$type = 4;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 	}
 	/**
      * @depends test_acp_edit_clean_for_event_medals
@@ -225,34 +225,34 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->assertEquals(4, $this->medals_for_event($this->get_topic_id('Test Topic 1')));
 		$this->assertEquals(1, $this->medals_for_user($this->get_user_id('testuser3')));
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('MEDALS_EDIT_STEP_TWO_EVENT', $crawler->filter('html')->text());
-		
+
 		$this->assertEquals(1, $this->medals_for_user($this->get_user_id('testuser3')));
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['users'] = array($this->get_user_id('testuser3') => array('delete' => 1));
 
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->filter('html')->text());
-		
+
 		$this->assertEquals(0, $this->medals_for_user($this->get_user_id('testuser3')));
-		
+
 		$this->assertEquals(3, $this->medals_for_event($this->get_topic_id('Test Topic 1')));
-		
+
 		$this->logout();
 	}
 	/**
@@ -262,27 +262,27 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->assertEquals(2, $this->medal_type($this->get_user_id('testuser1'), $this->get_topic_id('Test Topic 1')));
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['users'] = array($this->get_user_id('testuser1') => array('select' => 3));
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->filter('html')->text());
 		$this->assertEquals(3, $this->medal_type($this->get_user_id('testuser1'), $this->get_topic_id('Test Topic 1')));
-		
+
 		$this->logout();
 	}
 	/**
@@ -293,44 +293,44 @@ class eventmedals_acp_edit_test extends eventmedals_base
 		$this->assertContains('none', $this->medal_image($this->get_user_id('testuser1'), $this->get_topic_id('Test Topic 1')));
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['image'] = '/images/test/data/image.jpg';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->filter('html')->text());
-		
+
 		$this->assertContains('/images/test/data/image.jpg', $this->medal_image($this->get_user_id('testuser1'), $this->get_topic_id('Test Topic 1')));
-		
+
 		//let's test (and go back to none as image)
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['image'] = 'none';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->filter('html')->text());
-		
+
 		$this->assertContains('none', $this->medal_image($this->get_user_id('testuser1'), $this->get_topic_id('Test Topic 1')));
-		
+
 		$this->logout();
 	}
 	/**
@@ -340,58 +340,58 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['day'] = 2;
 		$form['month'] = 6;
 		$form['year'] = 2013;
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->filter('html')->text());
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['day'] = 2;
 		$form['month'] = 6;
 		$form['year'] = 1969;
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->assertContainsLang('ERR_DATE_ERR', $crawler->filter('html')->text());
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['day'] = 2;
 		$form['month'] = 5;
 		$form['year'] = 2014;
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->logout();
 	}
 	/**
@@ -401,46 +401,46 @@ class eventmedals_acp_edit_test extends eventmedals_base
 	{
 		$this->login();
 		$this->admin_login();
-		
+
 		$this->add_lang_ext('anavaro/eventmedals', 'info_acp_eventmedals');
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['target_event_new'] = 9999;
-	
+
 		$crawler = self::submit($form);
 		$this->assertContainsLang('ERR_TOPIC_ERR', $crawler->text());
-		
-		
+
+
 		// Test creating topic and post to test
 		$this->post = $this->create_topic(2, 'Test Topic 2', 'This is a test topic posted by the testing framework.');
 		$crawler = self::request('GET', "viewtopic.php?t={$this->post['topic_id']}&sid={$this->sid}");
-		
+
 		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-eventmedals-acp-main_module&mode=edit&sid=' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['topic'] = $this->get_topic_id('Test Topic 1');
 		$form['event_edit_type'] = 'event';
-		
+
 		$crawler = self::submit($form);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		$form['target_event_new'] = $this->get_topic_id('Test Topic 2');;
-	
+		$form['target_event_new'] = $this->get_topic_id('Test Topic 2');
+
 		$crawler = self::submit($form);
 		$this->assertContainsLang('SUCCESS_EDIT_INFO', $crawler->text());
-		
+
 		$this->logout();
 	}
 }
--- tests/functional/eventmedals_view_tests.php
+++ tests/functional/eventmedals_view_tests.php
@@ -19,35 +19,35 @@ class eventmedals_view_tests extends eventmedals_base
 	public function test_prepare()
 	{
 		$this->clean_medals_db();
-		
+
 		$owner_id = $this->get_user_id('admin');
 		$type = 1;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser1');
 		$type = 2;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser2');
 		$type = 3;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 		$owner_id = $this->get_user_id('testuser3');
 		$type = 4;
 		$link = $this->get_topic_id('Test Topic 1');
 		$date = 1399248000;
-		
+
 		$this->assertEquals(1, $this->set_medal($owner_id, $type, $link, $date));
-		
+
 	}
 	/**
      * @depends test_prepare
@@ -55,14 +55,14 @@ class eventmedals_view_tests extends eventmedals_base
 	public function test_user_viewtopic_medals()
 	{
 		$this->login('testuser1');
-		
+
 		$crawler = self::request('GET', "viewtopic.php?t={$this->get_topic_id('Test Topic 1')}&sid={$this->sid}");
-		
+
 		$this->assertContains('x 1', $crawler->filter('.medals_postrow')->text());
-		
+
 		$this->logout();
 	}
-	
+
 	/**
      * @depends test_user_viewtopic_medals
      */
@@ -70,17 +70,17 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login();
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'ucp.php?i=-anavaro-eventmedals-ucp-ucp_medals_module&mode=control' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['ucp_profile_view'] = 0;
-		
+
 		$crawler = self::submit($form);
-		
+
 		$this->logout();
 	}
-	
+
 	/**
      * @depends test_change_acl_to_none
      */
@@ -88,13 +88,13 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login('testuser1');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('UCP_PROFILE_ACC_ERROR', $crawler->filter('html')->text());
 		$this->logout();
 	}
-	
+
 	/**
      * @depends test_view_acl_none
      */
@@ -102,17 +102,17 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login();
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'ucp.php?i=-anavaro-eventmedals-ucp-ucp_medals_module&mode=control' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['ucp_profile_view'] = 1;
-		
+
 		$crawler = self::submit($form);
-		
+
 		//add testuser2 as enemy
-		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser2');
-		
+		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser2'));
+
 		$this->logout();
 	}
 	/**
@@ -122,22 +122,22 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login('testuser2');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('UCP_PROFILE_ACC_ERROR', $crawler->filter('html')->text());
 		$this->logout();
-		
+
 		$this->login('testuser1');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('MEDAL_TYPE_ONE', $crawler->filter('#medals_show')->text());
 		$this->logout();
-		
+
 	}
-	
+
 	/**
      * @depends test_view_acl_none
      */
@@ -145,20 +145,20 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login();
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'ucp.php?i=-anavaro-eventmedals-ucp-ucp_medals_module&mode=control' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['ucp_profile_view'] = 2;
-		
+
 		$crawler = self::submit($form);
-		
+
 		//add testuser1 as frind
-		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser1');
-		
+		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser1'));
+
 		$this->logout();
 	}
-	
+
 	/**
      * @depends test_change_acl_to_all
      */
@@ -166,29 +166,29 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login('testuser1');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('MEDAL_TYPE_ONE', $crawler->filter('#medals_show')->text());
 		$this->logout();
-		
+
 		$this->login('testuser2');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('MEDAL_TYPE_ONE', $crawler->filter('html')->text());
 		$this->logout();
-		
+
 		$this->login('testuser3');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('MEDAL_TYPE_ONE', $crawler->filter('#medals_show')->text());
 		$this->logout();
 	}
-	
+
 	/**
      * @depends test_view_acl_all
      */
@@ -196,17 +196,17 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login();
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'ucp.php?i=-anavaro-eventmedals-ucp-ucp_medals_module&mode=control' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['ucp_profile_view'] = 3;
-		
+
 		$crawler = self::submit($form);
-		
+
 		//add testuser1 as frind
-		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser1');
-		
+		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser1'));
+
 		$this->logout();
 	}
 	/**
@@ -216,29 +216,29 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login('testuser1');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('MEDAL_TYPE_ONE', $crawler->filter('#medals_show')->text());
 		$this->logout();
-		
+
 		$this->login('testuser2');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('UCP_PROFILE_ACC_ERROR', $crawler->filter('html')->text());
 		$this->logout();
-		
+
 		$this->login('testuser3');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('admin') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('UCP_PROFILE_ACC_ERROR', $crawler->filter('#medals_show')->text());
 		$this->logout();
 	}
-	
+
 	/**
      * @depends test_view_acl_friends
      */
@@ -246,53 +246,53 @@ class eventmedals_view_tests extends eventmedals_base
 	{
 		$this->login('testuser3');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'ucp.php?i=-anavaro-eventmedals-ucp-ucp_medals_module&mode=control' . $this->sid);
-		
+
 		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
 		$form['ucp_profile_view'] = 0;
-		
+
 		$crawler = self::submit($form);
-		
+
 		//add testuser1 as frind
-		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser1');
-		
+		$this->add_foe($this->user->data['user_id'], $this->get_user_id('testuser1'));
+
 		$this->logout();
 	}
 	/**
      * @depends test_change_acl_to_none_force_admin
      */
-	public function test_view_acl_friends()
+	public function test_view_acl_friends_1()
 	{
 		$this->login('testuser1');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('testuser3') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('UCP_PROFILE_ACC_ERROR', $crawler->filter('#medals_show')->text());
 		$this->logout();
-		
+
 		$this->login('testuser2');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('testuser3') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('UCP_PROFILE_ACC_ERROR', $crawler->filter('html')->text());
 		$this->logout();
-		
+
 		$this->login('testuser3');
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('testuser3') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('MEDAL_TYPE_FOUR', $crawler->filter('#medals_show')->text());
 		$this->logout();
-		
+
 		$this->login();
 		$this->add_lang_ext('anavaro/eventmedals', 'event_medals');
-		
+
 		$crawler = self::request('GET', 'memberlist.php?mode=viewprofile&u=' . $this->get_user_id('testuser3') . '&sid=' . $this->sid);
-		
+
 		$this->assertContainsLang('MEDAL_TYPE_FOUR', $crawler->filter('#medals_show')->text());
 		$this->logout();
 	}
